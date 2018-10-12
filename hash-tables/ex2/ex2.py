a9647cd10ad04aa18d49e8e66690077c343a17cb
M.Real <mreal126@protonmail.com>
2018-10-12 14:17:53 -0400

    ex2 passes

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,5 +1,15 @@
 def reconstruct_trip(tickets):
-  pass 
+  hash = {}
+  plans = []
+  for ticket in tickets:
+    hash[ticket[0]] = ticket[1]
+  status = hash[None]
+  while status != None:
+    plans.append(status)
+    if status not in hash:
+      return []
+    status = hash[status]
+  return plans
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
